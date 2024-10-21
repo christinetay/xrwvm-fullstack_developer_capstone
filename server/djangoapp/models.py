a7665dcde2851803789d26d2585ee9bec8c27c24
christinetay <christinetay22@gmail.com>
2024-10-21 05:55:31 -0400

    CI/CI changes of djangoapp-models.py in module-lab-4(2)

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -34,8 +34,21 @@ class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     dealer_id = models.TextField()
     name = models.CharField(max_length=250)
-    type = models.CharField(max_length=10,choices=[('SEDAN','Sedan'), ('SUV','SUV'), ('WAGON','Wagon')])
-    year = models.IntegerField(default=2023,validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+    type = models.CharField(
+        max_length=10,
+        choices=[
+            ('SEDAN','Sedan'), 
+            ('SUV','SUV'), 
+            ('WAGON','Wagon')
+        ]
+    )
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MinValueValidator(2015), 
+            MaxValueValidator(2023)
+        ]
+    )
 
     def __str__(self):
         return self.name
