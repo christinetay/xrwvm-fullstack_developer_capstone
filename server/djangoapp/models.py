222cfe797ba3372470ee8d0655c2834121bf24f9
christinetay <christinetay22@gmail.com>
2024-10-21 04:18:18 -0400

    CI/CI changes of djangoapp-models.py in module-lab-4

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -34,12 +34,8 @@ class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     dealer_id = models.TextField()
     name = models.CharField(max_length=250)
-    type = models.CharField(max_length=10, 
-        choices=[('SEDAN','Sedan'), ('SUV','SUV'), ('WAGON','Wagon')])
-    year = models.IntegerField(default=2023,
-        validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+    type = models.CharField(max_length=10,choices=[('SEDAN','Sedan'), ('SUV','SUV'), ('WAGON','Wagon')])
+    year = models.IntegerField(default=2023,validators=[MinValueValidator(2015), MaxValueValidator(2023)])
 
     def __str__(self):
         return self.name
-
-
