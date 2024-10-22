28e2b25dee0235f1b35675c674f5d86aa6ce2784
christinetay <christinetay22@gmail.com>
2024-10-22 10:05:34 +0800

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -136,16 +136,19 @@ def add_review(request):
             "status": 403,
             "message": "Unauthorized"
         })
-    
+
 
 # Create a 'get_cars' view to get car details
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if count == 0:
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
     return JsonResponse({"CarModels": cars})
